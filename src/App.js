596d2c614b14abc43011eb0230ce5b255c3959d8
chriscoded <amaemechris@gmail.com>
2024-12-20 09:28:12 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import {Col, Row, Container} from 'react-bootstrap';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { LogLevel, HubConnectionBuilder } from '@microsoft/signalr';
+import React, { useState } from 'react';
+import WaitingRoom from './components/waitingroom'; // Adjust the path as needed
+
 
 
 function App() {
+
+    const[conn, setConnection] = useState();
+
+    const joinChatRoom  = async (username, chatroom) => {
+       try {
+          //  initiate a connection
+          const conn = HubConnectionBuilder()
+                        .withUrl("http://localhost:5230")
+                        .configureLogging(LogLevel.Information)
+                        .build();
+
+        // set up
+        conn.on("JoinSpecificChatRoom", (username, msg) =>{
+          console.log("msg: ", msg);
+
+        })
+       } catch (error) {
+          console.log(error);
+       }
+    }
+
   return (
     <div >
       <main>
         <Container>
             <Row class='px-5 my-5'>
-              <Col sm='2'>
-                <h1 className='font-weight-light'>Welcome to F1 ChatAppS</h1>
+              <Col sm='12'>
+                <h1 className='font-weight-light'>Welcome to F1 ChatApp</h1>
               </Col>
             </Row>
+            <WaitingRoom></WaitingRoom>
         </Container>
       </main>
     </div>
