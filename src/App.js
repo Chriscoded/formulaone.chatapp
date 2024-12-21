cdddd06a297f171ef3b8e46479afdb1912e8df13
chriscoded <amaemechris@gmail.com>
2024-12-21 09:06:48 +0100

    send and receive group messages

--- src/App.js
+++ src/App.js
@@ -4,29 +4,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { LogLevel, HubConnectionBuilder } from '@microsoft/signalr';
 import React, { useState } from 'react';
 import WaitingRoom from './components/waitingroom'; // Adjust the path as needed
+import ChatRoom from './components/ChatRoom';
 
 
 
 function App() {
 
     const[conn, setConnection] = useState();
+    const[messages, setMessages] = useState([]);
 
     const joinChatRoom  = async (username, chatroom) => {
        try {
           //  initiate a connection
-          const conn = HubConnectionBuilder()
-                        .withUrl("http://localhost:5230")
+          const conn = new HubConnectionBuilder()
+                        .withUrl("https://localhost:44326/chat")
                         .configureLogging(LogLevel.Information)
                         .build();
 
         // set up
-        conn.on("JoinSpecificChatRoom", (username, msg) =>{
+        conn.on("ReceiveMessage", (username, msg) =>{
           console.log("msg: ", msg);
+          setMessages(messages => [...messages, {username,msg}])
+        });
 
-        })
+        conn.on("ReceiveSpecificMessage", (username, msg) =>{
+          setMessages(messages => [...messages, {username,msg}])
+        });
+
+        await conn.start();
+        await conn.invoke("JoinSpecificChatRoom", {username, chatroom});
+
+        setConnection(conn);
+
        } catch (error) {
           console.log(error);
        }
+    }
+
+    const sendMessage = async(message) =>{
+      try {
+        await conn.invoke("SendMessage", message);
+      } catch (error) {
+        console.log(error)
+      }
     }
 
   return (
@@ -38,7 +58,11 @@ function App() {
                 <h1 className='font-weight-light'>Welcome to F1 ChatApp</h1>
               </Col>
             </Row>
-            <WaitingRoom></WaitingRoom>
+            { !conn
+              ? <WaitingRoom joinChatRoom={joinChatRoom}></WaitingRoom>
+              : <ChatRoom messages={messages} sendMessage={sendMessage}></ChatRoom>
+            }
+            
         </Container>
       </main>
     </div>
